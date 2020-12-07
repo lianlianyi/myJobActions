429828f2a00b2e02788ec3eba3f03fe671248a59
lianlianyi <lianlianyi@vip.qq.com>
2020-12-07 12:36:30 +0800

    不要卡死进程

--- src/main/java/cc/chenwenxi/actions/Run.java
+++ src/main/java/cc/chenwenxi/actions/Run.java
@@ -2,7 +2,6 @@ package cc.chenwenxi.actions;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Console;
-import cn.hutool.core.thread.ThreadUtil;
 
 import java.util.Date;
 
@@ -12,16 +11,17 @@ import java.util.Date;
 public class Run {
     public static void main(String[] args) {
         Console.log("另开线程,不要占用主线程");
-        ThreadUtil.execAsync(()->{
-            try{
-                while(true){
-                    System.out.println("执行成功:"+ DateUtil.formatDateTime(new Date()));
-                    ThreadUtil.sleep(2000);
-                }
-            }catch(Exception e){
-               e.printStackTrace();
-            }
-
-        });
+        System.out.println("执行成功:"+ DateUtil.formatDateTime(new Date()));
+//        ThreadUtil.execAsync(()->{
+//            try{
+//                while(true){
+//                    System.out.println("执行成功:"+ DateUtil.formatDateTime(new Date()));
+//                    ThreadUtil.sleep(2000);
+//                }
+//            }catch(Exception e){
+//               e.printStackTrace();
+//            }
+//
+//        });
     }
 }
