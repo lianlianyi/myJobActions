692dec800156a611aae728813785881ce4334251
lianlianyi <lianlianyi@vip.qq.com>
2020-12-07 10:27:09 +0800

    单开线程

--- src/main/java/cc/chenwenxi/actions/Run.java
+++ src/main/java/cc/chenwenxi/actions/Run.java
@@ -1,14 +1,24 @@
 package cc.chenwenxi.actions;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ThreadUtil;
+
 /**
  * @author chenwenxi
  */
 public class Run {
     public static void main(String[] args) {
-        while(true){
-            System.out.println("执行成功");
+        Console.log("另开线程,不要占用主线程");
+        ThreadUtil.execAsync(()->{
+            try{
+                while(true){
+                    System.out.println("执行成功");
 
-        }
+                }
+            }catch(Exception e){
+               e.printStackTrace();
+            }
 
+        });
     }
 }
